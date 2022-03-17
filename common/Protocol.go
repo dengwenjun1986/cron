8a039618e7a74917986683d1bce7c8ae12883538
dengwj <dengwenjun1986@163.com>
2022-03-17 23:26:00 +0800

    json file

--- common/Protocol.go
+++ common/Protocol.go
@@ -7,21 +7,26 @@ import (
 
 // 任务
 type Job struct {
-	Name string `json:"name"`  // 任务名
-	Command string `json:"command"` // shell命令
+	Name     string `json:"name"`     // 任务名
+	Command  string `json:"command"`  // shell命令
 	CronExpr string `json:"cronExpr"` // cron表达式
 }
 
 // HTTP接口应答
 type Response struct {
-	Errno int `json:"errno"`
-	Msg string `json:"msg"`
-	Data interface{} `json:"data"`
+	Errno int         `json:"errno"`
+	Msg   string      `json:"msg"`
+	Data  interface{} `json:"data"`
 }
 
+// 变化事件
+type JobEvent struct {
+	EventType int // SAVE DELETE
+	job       *Job
+}
 
 // 应答方法
-func BuildResp(errno int,msg string,data interface{})(resp []byte,err error){
+func BuildResp(errno int, msg string, data interface{}) (resp []byte, err error) {
 	//1.定义一个response
 	var (
 		response Response
@@ -31,18 +36,18 @@ func BuildResp(errno int,msg string,data interface{})(resp []byte,err error){
 	response.Data = data
 
 	// 2.序列化json
-	resp,err = json.Marshal(response)
+	resp, err = json.Marshal(response)
 
 	return
 }
 
 // 反序列化Job
-func UnpackJob(value []byte)(ret *Job,err error){
+func UnpackJob(value []byte) (ret *Job, err error) {
 	var (
 		job *Job
 	)
 	job = &Job{}
-	if err = json.Unmarshal(value,job);err != nil {
+	if err = json.Unmarshal(value, job); err != nil {
 		return
 	}
 	ret = job
@@ -50,7 +55,16 @@ func UnpackJob(value []byte)(ret *Job,err error){
 }
 
 // 从etcd的key中提取任务名
-func ExtratJobName(JobKey string)(string){
+func ExtratJobName(JobKey string) string {
 	// /cron/jobs/job10 删掉 /cron/jobs
-	return strings.TrimPrefix(JobKey,JOB_SAVE_DIR)
+	return strings.TrimPrefix(JobKey, JOB_SAVE_DIR)
+}
+
+// 任务变化事件 1）更新任务  2）删除任务
+func BuildEvent(eventType int, job *Job) (jobEvent *JobEvent) {
+
+	return &JobEvent{
+		EventType: eventType,
+		job:       job,
+	}
 }
--- worker/JobMgr.go
+++ worker/JobMgr.go
@@ -6,13 +6,13 @@ import (
 	"go.etcd.io/etcd/api/v3/mvccpb"
 	clientv3 "go.etcd.io/etcd/client/v3"
 	"time"
-	)
+)
 
 // 任务管理器
 type JobMgr struct {
-	client *clientv3.Client
-	kv clientv3.KV
-	lease clientv3.Lease
+	client  *clientv3.Client
+	kv      clientv3.KV
+	lease   clientv3.Lease
 	watcher clientv3.Watcher
 }
 
@@ -22,24 +22,25 @@ var (
 )
 
 // 监听任务变化
-func(jobMgr *JobMgr)watchJobs()(err error){
+func (jobMgr *JobMgr) watchJobs() (err error) {
 	var (
-		getResp *clientv3.GetResponse
-		kvpair *mvccpb.KeyValue
-		job *common.Job
+		getResp            *clientv3.GetResponse
+		kvpair             *mvccpb.KeyValue
+		job                *common.Job
 		watchStartRevision int64
-		watchChan clientv3.WatchChan
-		watchResp clientv3.WatchResponse
-		watchEvent *clientv3.Event
-		jobName string
+		watchChan          clientv3.WatchChan
+		watchResp          clientv3.WatchResponse
+		watchEvent         *clientv3.Event
+		jobName            string
+		jobEvent           *common.JobEvent
 	)
 	// 1.get 一下/cron/jobs/目录下的所有任务，并且获知当前集群的revision
-	if getResp,err = jobMgr.kv.Get(context.TODO(),common.JOB_SAVE_DIR,clientv3.WithPrevKV()); err != nil {
+	if getResp, err = jobMgr.kv.Get(context.TODO(), common.JOB_SAVE_DIR, clientv3.WithPrevKV()); err != nil {
 		return
 	}
-	for _,kvpair = range getResp.Kvs {
+	for _, kvpair = range getResp.Kvs {
 		// 反序列化json得到job
-		if job,err = common.UnpackJob(kvpair.Value);err != nil {
+		if job, err = common.UnpackJob(kvpair.Value); err != nil {
 			// TODO: 是把这个job同步给scheduler（调度协程）
 		}
 	}
@@ -49,57 +50,52 @@ func(jobMgr *JobMgr)watchJobs()(err error){
 		// 从GET时刻的后续版本监听变化
 		watchStartRevision = getResp.Header.Revision + 1
 		// 监听/cron/jobs/目录的后续变化
-		watchChan = jobMgr.watcher.Watch(context.TODO(),common.JOB_SAVE_DIR,clientv3.WithRev(watchStartRevision))
+		watchChan = jobMgr.watcher.Watch(context.TODO(), common.JOB_SAVE_DIR, clientv3.WithRev(watchStartRevision))
 
 		// 处理监听事件
 		for watchResp = range watchChan {
-			for watchEvent = range watchResp.Events {
+			for _, watchEvent = range watchResp.Events {
 				switch watchEvent.Type {
 				case mvccpb.PUT: //任务保存事件
-					if job,err = common.UnpackJob(watchEvent.Kv.Value);err != nil {
+					if job, err = common.UnpackJob(watchEvent.Kv.Value); err != nil {
 						continue
 					}
-					//构造一个Event事件
-
-
-
-
-					// TODO:反序列化Job,推给scheduler
+					//构造一个更新Event事件
+					jobEvent = common.BuildJobEvent(common.JOB_EVENT_SAVE, job)
 
 				case mvccpb.DELETE: //任务被删除了
 					// Delete /cron/jobs/job10
-					jobName = common.
+					jobName = common.ExtratJobName(string(watchEvent.Kv.Key))
 
-					// TODO:推一个删除事件给scheduler
+					job = &common.Job{Name: jobName}
+					//构造一个删除Event事件
+					jobEvent = common.BuildJobEvent(common.JOB_EVENT_DELETE, job)
 				}
 			}
 		}
 	}()
 
-
 	return
 }
 
-
-
 // 初始化管理器
-func InitJobMgr()(err error){
+func InitJobMgr() (err error) {
 
 	var (
-		config clientv3.Config
-		client *clientv3.Client
-		kv clientv3.KV
-		lease clientv3.Lease
+		config  clientv3.Config
+		client  *clientv3.Client
+		kv      clientv3.KV
+		lease   clientv3.Lease
 		watcher clientv3.Watcher
 	)
 
 	// 初始化配置
 	config = clientv3.Config{
-		Endpoints:G_config.EtcdEndpoints, // 集群地址
+		Endpoints:   G_config.EtcdEndpoints,                                     // 集群地址
 		DialTimeout: time.Duration(G_config.EtcdDialtimeout) * time.Millisecond, // 链接超时
 	}
 	//建立链接
-	if client,err = clientv3.New(config);err !=nil {
+	if client, err = clientv3.New(config); err != nil {
 		return
 	}
 
@@ -110,18 +106,11 @@ func InitJobMgr()(err error){
 
 	// 赋值单例
 	G_jobMgr = &JobMgr{
-		client: client,
-		kv: kv,
-		lease: lease,
+		client:  client,
+		kv:      kv,
+		lease:   lease,
 		watcher: watcher,
 	}
 
 	return
 }
-
-
-
-
-
-
-
